8ae4fc754ca941aaa198af9b7056daaaa791626b
Igor Minar <igor@angularjs.org>
2013-11-11 02:43:30 +0100

    buttons-radio.js

---
+++ src/ngmodelctrl_buttons/exercise/buttons-radio.js
@@ -0,0 +1,12 @@
+angular.module('bs.buttons-radio', [])
+
+  .directive('bsBtnRadio', function () {
+
+    return {
+      require: 'ngModel',
+      link: function (scope, element, attrs, ngModelCtrl) {
+
+
+      }
+    };
+  });
